64aace77c20f89dd22c188f24832ace3d1aed250
Aldor007 <aldor007@gmail.com>
2014-08-26 22:34:25 +0200

    netlogon scrips

---
+++ roles/samba/files/netlogon/grupa_C.bat
@@ -0,0 +1,4 @@
+net time \\SALA37 /set /yes
+net use x: \\SALA37\homes
+net use z: \\SALA37\informatyka
+net use y: \\SALA37\grupa_A
---
+++ roles/samba/files/netlogon/logon.bat
@@ -0,0 +1,4 @@
+net use x: \\SALA37\homes
+net use z: \\SALA37\informatyka
+net use y: \\SALA37\prace
+net time \\SALA37 /set /yes
